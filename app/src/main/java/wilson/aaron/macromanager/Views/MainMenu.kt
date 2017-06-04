ceab9cb5f8c0d77efcb1f3ca5a253f90c3e2cc3a
Aaron Wilson <aaronnw11@gmail.com>
2017-06-03 23:04:38 -0500

    Initialize project

---
+++ app/src/main/java/wilson/aaron/macromanager/Views/MainMenu.kt
@@ -0,0 +1,16 @@
+package wilson.aaron.macromanager.Views
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+import wilson.aaron.macromanager.R
+
+class MainMenu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+        //Test comment
+    }
+}
